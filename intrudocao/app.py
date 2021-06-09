782b595a6a76afb278f05209c6fb901eaaf495a3
Christopher <rpa@one4.com.br>
2021-06-09 15:52:21 -0300

    resquest, redirect, url_for

--- intrudocao/app.py
+++ intrudocao/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import re
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,5 +7,25 @@ app = Flask(__name__)
 def index():
     return "Página Inicial"
 
+@app.route("/redirect")
+def redirecting():
+    return redirect(url_for("index"))
+
+
+@app.route("/posts/<int:id>")
+def posts(id):
+    title = request.args.get("title")
+
+    data = dict(
+        path=request.path,
+        content_type=request.content_type,
+        referrer=request.referrer,
+        method=request.method,
+        title=title,
+        id=id if id else 0
+    )
+
+    return data
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
