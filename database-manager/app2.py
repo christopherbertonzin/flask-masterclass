9625958fa6482af628945e94e461138c98b8110c
Christopher <rpa@one4.com.br>
2021-06-15 23:38:07 -0300

    add route user

--- database-manager/app2.py
+++ database-manager/app2.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -9,17 +8,20 @@ db = SQLAlchemy(app)
 
 # ================== Models ==================
 
+
 class User(db.Model):
-    #__tablename__ = "users"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(84), nullable=False)
     email = db.Column(db.String(84), nullable=False, unique=True, index=True)
-    password = db.Column(db.String(255), nullable = False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __repr__(self) -> str:
         return self.name
 
+
 # ================== Routes ==================
+
 
 @app.route("/")
 def index():
@@ -27,12 +29,19 @@ def index():
     return render_template("index.html", users=users)
 
 
-@app.route("/users/delete/<int:id>")
+@app.route("/user/<int:id>")
+def unique(id):
+    user = User.query.get(id)
+    return render_template("user.html", user=user)
+
+
+@app.route("/user/delete/<int:id>")
 def delete(id):
     user = User.query.filter_by(id=id).first()
     db.session.delete(user)
     db.session.commit()
-    return redirect('/')
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
