871240f9c61e0af99cdcdccdc44d5847997871fe
Christopher <rpa@one4.com.br>
2021-06-21 23:22:43 -0300

    add register route

--- database-manager/app2.py
+++ database-manager/app2.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 # ================== Models ==================
+@login_manager.user_loader
+def current_user(user_id):
+    return User.query.get(user_id)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(84), nullable=False)
     email = db.Column(db.String(84), nullable=False, unique=True, index=True)
     password = db.Column(db.String(255), nullable=False)
+    profile = db.relationship("Profile", backref="user", uselist=False)
+
+    def __repr__(self) -> str:
+        return self.name
+
+
+class Profile(db.Model):
+    __tablename__ = "profiles"
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.Unicode(124), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     def __repr__(self) -> str:
         return self.name
@@ -30,6 +47,7 @@ def index():
 
 
 @app.route("/user/<int:id>")
+# @login_required
 def unique(id):
     user = User.query.get(id)
     return render_template("user.html", user=user)
@@ -41,6 +59,28 @@ def delete(id):
     db.session.delete(user)
     db.session.commit()
     return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    user = User()
+
+    if request.method == "POST":
+        user.name = request.form["name"]
+        user.email = request.form["email"]
+        user.password = generate_password_hash(request.form["password"])
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    pass
 
 
 if __name__ == "__main__":
